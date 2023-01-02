15c13d7b11f0981a1b2e91f9bd9977e97ba941d9
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-02 13:09:20 +0100

    refactor: make boilerplate

--- src/newtab/index.tsx
+++ src/newtab/index.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import TimeZones from "../components/TimeZones";
 
 const NewTab = () => {
   return (
     <>
-      <h1 className="text-xl">Hello new tab! !!!</h1>
-      <TimeZones />
-      <img src="img/bg_1.png" alt="bg" />
+      <h1>Hello World!</h1>
     </>
   );
 };
