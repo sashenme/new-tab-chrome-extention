2c6a3727d78f137d9cef3dd148ddf64c08bb8a1e
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-06 20:49:50 +0100

    refactor: format  code base

--- src/components/TimeZones/TimeSlot.tsx
+++ src/components/TimeZones/TimeSlot.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState, useRef} from "react";
+import React, { useEffect, useState, useRef } from "react";
 import moment from "moment-timezone";
+import { dateFormat, timeFormat } from "../../utils/constants";
+import { countryImgApi } from "../../utils/apis";
 
 interface TimeSlotProps {
   city: string;
@@ -7,14 +9,10 @@ interface TimeSlotProps {
   country: string;
 }
 
-const TimeSlot: React.FC<TimeSlotProps> = ({
-  city,
-  timezone,
-  country,
-}) => {
+const TimeSlot: React.FC<TimeSlotProps> = ({ city, timezone, country }) => {
   const [timeDiff, setTimeDiff] = useState("");
   const [time, setTime] = useState("");
-  const [date, setDate] = useState("")
+  const [date, setDate] = useState("");
 
   const getTimeDifference = (timezone) => {
     var now: any = moment.utc();
@@ -23,7 +21,7 @@ const TimeSlot: React.FC<TimeSlotProps> = ({
     var currentTimezone = moment.tz.zone(guessed).utcOffset(now);
     var newTimezone = moment.tz.zone(timezone).utcOffset(now);
     // calculate the difference in hours
-    const difference = (currentTimezone - newTimezone) / 60; 
+    const difference = (currentTimezone - newTimezone) / 60;
 
     return difference > 0 ? `+${difference}` : difference;
   };
@@ -31,43 +29,35 @@ const TimeSlot: React.FC<TimeSlotProps> = ({
   var n = useRef(null);
   var t = useRef(null);
 
-  const showSeconds = false;
-  const timeFormat = showSeconds ? "hh:mm:ss A" : "hh:mm A" ;
-  const dateFormat = "YYYY.MM.DD"
-
-
-  
-
-  const getTheDay = date => {
-    const yesterday  = moment().subtract(1, 'days').format(dateFormat);
+  const getTheDay = (date) => {
+    const yesterday = moment().subtract(1, "days").format(dateFormat);
     const today = moment().format(dateFormat);
-    const tomorrow = moment().add(1, 'days').format(dateFormat);
-    
-    switch(date){
+    const tomorrow = moment().add(1, "days").format(dateFormat);
+
+    switch (date) {
       case today:
-        return 'Today';
+        return "Today";
       case tomorrow:
-        return 'Tomorrow';
+        return "Tomorrow";
       case yesterday:
-        return 'Yesterday';
+        return "Yesterday";
       default:
-        return 'NaN'
+        return "NaN";
     }
-  }
+  };
 
   useEffect(() => {
     setTimeDiff(getTimeDifference(timezone));
-    const now =moment.tz(moment(), timezone); 
-    setTime(now.format(timeFormat))
+    const now = moment.tz(moment(), timezone);
     const timeZoneDate = now.format(dateFormat);
-  
-    setDate(getTheDay(timeZoneDate));
-    console.log()
     const timeOffset = 1000 - new Date(Date.now()).getMilliseconds();
-    
+
+    setTime(now.format(timeFormat));
+    setDate(getTheDay(timeZoneDate));
+
     n.current = setTimeout(() => {
       t.current = setInterval(() => {
-        setTime(moment.tz(moment(), timezone).format(timeFormat))
+        setTime(moment.tz(moment(), timezone).format(timeFormat));
       });
     }, timeOffset);
 
@@ -75,17 +65,23 @@ const TimeSlot: React.FC<TimeSlotProps> = ({
       n.current = null;
       clearInterval(t.current);
     };
-
   }, [timezone]);
 
   return (
     <div className="text-center text-white bg-gradient-to-bl from-slate-300/30 to-slate-100/10 px-6 py-4 rounded-md backdrop-blur-sm border-solid border border-white/20">
-      <div className="text-2xl opacity-70 mb-1 flex gap-2 items-center justify-center">
-        <img src={`https://countryflagsapi.com/svg/${country}`} crossOrigin="anonymous" alt=""  className="h-4"/>
-        {city}
+      <div className="text-2xl  mb-1 flex gap-2 items-center justify-center">
+        <img
+          src={`${countryImgApi}/${country}`}
+          crossOrigin="anonymous"
+          alt=""
+          className="h-4"
+        />
+        <span className="opacity-70">{city}</span>
       </div>
       <h1 className="text-4xl font-bold">{time}</h1>
-      <div className="text-sm">{date}, {timeDiff}</div>
+      <div className="text-sm">
+        {date}, {timeDiff}
+      </div>
     </div>
   );
 };
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,4 @@
 export const unit = { name: "metric", temperature: "°C" };
+export const showSeconds = false;
+export const timeFormat = showSeconds ? "hh:mm:ss A" : "hh:mm A" ;
+export const dateFormat = "YYYY.MM.DD";
