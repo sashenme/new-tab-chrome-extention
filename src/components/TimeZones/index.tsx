614ecdf376647e10d715930800bbc306df77360d
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-06 20:39:39 +0100

    style: change to grid cols

--- src/components/TimeZones/index.tsx
+++ src/components/TimeZones/index.tsx
@@ -42,7 +42,7 @@ export default function TimeZones() {
   ];
 
   return (
-    <div className="flex gap-8 justify-between my-8">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 justify-between my-8">
       {favoriteList.map((favorite) => (
         <TimeSlot
           key={favorite.id}
