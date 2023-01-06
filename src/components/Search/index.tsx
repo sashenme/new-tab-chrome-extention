8791fc737111507a949812d05e69fecb8954aeef
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-06 22:45:35 +0100

    style: search bar

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,20 +1,24 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import SearchIcon from "../../assets/icons/search";
 
 const Search = () => {
-  const [text,setText] = useState("");
-  const handleKeyPress = event =>{
-    if(event.key === 'Enter'){
-      window.open(`https://www.google.com/search?q=${text}`, '_blank');
+  const [text, setText] = useState("");
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      window.open(`https://www.google.com/search?q=${text}`, "_blank");
     }
-  }
+  };
   return (
     <div>
-      <div
-        className="rounded-full px-6  text-[16px] text-white bg-white/30 backdrop-blur-sm flex items-center border border-white/30 gap-2 mt-20 cursor-text"      
-      >
+      <div className="rounded-full px-6  text-[16px] text-white bg-white/30 backdrop-blur-sm flex items-center border border-white/30 gap-2 mt-20 cursor-text">
         <SearchIcon />
-        <input type="search" className="bg-transparent h-full w-full py-4 outline-none" placeholder="Search Google or type a URL" onKeyPress={handleKeyPress} onChange={e=>setText(e.target.value)}/>
+        <input
+          type="search"
+          className="bg-transparent h-full w-full py-4 outline-none placeholder:text-white/70"
+          placeholder="Search Google or type a URL"
+          onKeyPress={handleKeyPress}
+          onChange={(e) => setText(e.target.value)}
+        />
       </div>
     </div>
   );
