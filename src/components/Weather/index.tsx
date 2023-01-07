ceda868eb95362052eef41b3531d2b6df5da73db
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-07 02:21:59 +0100

    refactor: get icon function to functions.js

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { openWeatherApi, openWeatherImg } from "../../utils/apis";
+import { openWeatherApi } from "../../utils/apis";
 import { unit } from "../../utils/constants";
+import {getWeatherIcon} from "../../utils/functions";
 
 interface Main {
   feels_like: number;
@@ -33,71 +34,6 @@ const Weather = () => {
     const body = await response.json();
     return body;
   }
-
-  const getWeatherIcon = (icon) => {
-    let iconName = "";
-
-    switch (icon) {
-      case "01d":
-        iconName = "clean-day";
-        break;
-      case "01n":
-        iconName = "clean-night";
-        break;
-      case "02d":
-        iconName = "partly-cloudy-day";
-        break;
-      case "02n":
-        iconName = "partly-cloudy-night";
-        break;
-      case "03d":
-        iconName = "cloud";
-        break;
-      case "03n":
-        iconName = "cloud";
-        break;
-      case "04d":
-        iconName = "doublecloud";
-        break;
-      case "04n":
-        iconName = "doublecloud";
-        break;
-      case "09d":
-        iconName = "shower-rain";
-        break;
-      case "09n":
-        iconName = "shower-rain";
-        break; 
-      case "10d":
-        iconName = "rain-day";
-        break;
-      case "10n":
-        iconName = "rain-night";
-        break; 
-      case "11d":
-        iconName = "thunder-rain";
-        break; 
-      case "11n":
-        iconName = "thunder-rain";
-        break; 
-      case "13d":
-        iconName = "snow";
-        break; 
-      case "13n":
-        iconName = "snow";
-        break; 
-      case "50d":
-        iconName = "hazzy";
-        break; 
-      case "50n":
-        iconName = "hazzy";
-        break; 
-      default:
-        iconName = "cloud";
-    }
- 
-    return iconName;
-  };
 
   useEffect(() => {
     const getWeather = async () => {
--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,5 +1,64 @@
-export  async function http<T>(request: RequestInfo): Promise<T> {
-  const response = await fetch(request);
-  const body = await response.json();
-  return body;
-}
+export const getWeatherIcon = (icon) => {
+  let iconName = "";
+
+  switch (icon) {
+    case "01d":
+      iconName = "clean-day";
+      break;
+    case "01n":
+      iconName = "clean-night";
+      break;
+    case "02d":
+      iconName = "partly-cloudy-day";
+      break;
+    case "02n":
+      iconName = "partly-cloudy-night";
+      break;
+    case "03d":
+      iconName = "cloud";
+      break;
+    case "03n":
+      iconName = "cloud";
+      break;
+    case "04d":
+      iconName = "doublecloud";
+      break;
+    case "04n":
+      iconName = "doublecloud";
+      break;
+    case "09d":
+      iconName = "shower-rain";
+      break;
+    case "09n":
+      iconName = "shower-rain";
+      break; 
+    case "10d":
+      iconName = "rain-day";
+      break;
+    case "10n":
+      iconName = "rain-night";
+      break; 
+    case "11d":
+      iconName = "thunder-rain";
+      break; 
+    case "11n":
+      iconName = "thunder-rain";
+      break; 
+    case "13d":
+      iconName = "snow";
+      break; 
+    case "13n":
+      iconName = "snow";
+      break; 
+    case "50d":
+      iconName = "hazzy";
+      break; 
+    case "50n":
+      iconName = "hazzy";
+      break; 
+    default:
+      iconName = "cloud";
+  }
+
+  return iconName;
+};
