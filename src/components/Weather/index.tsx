65e0b4579ab023ffb2130dbf57047d1e7b1da14b
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-07 02:19:05 +0100

    feat: override weather icons

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -34,6 +34,71 @@ const Weather = () => {
     return body;
   }
 
+  const getWeatherIcon = (icon) => {
+    let iconName = "";
+
+    switch (icon) {
+      case "01d":
+        iconName = "clean-day";
+        break;
+      case "01n":
+        iconName = "clean-night";
+        break;
+      case "02d":
+        iconName = "partly-cloudy-day";
+        break;
+      case "02n":
+        iconName = "partly-cloudy-night";
+        break;
+      case "03d":
+        iconName = "cloud";
+        break;
+      case "03n":
+        iconName = "cloud";
+        break;
+      case "04d":
+        iconName = "doublecloud";
+        break;
+      case "04n":
+        iconName = "doublecloud";
+        break;
+      case "09d":
+        iconName = "shower-rain";
+        break;
+      case "09n":
+        iconName = "shower-rain";
+        break; 
+      case "10d":
+        iconName = "rain-day";
+        break;
+      case "10n":
+        iconName = "rain-night";
+        break; 
+      case "11d":
+        iconName = "thunder-rain";
+        break; 
+      case "11n":
+        iconName = "thunder-rain";
+        break; 
+      case "13d":
+        iconName = "snow";
+        break; 
+      case "13n":
+        iconName = "snow";
+        break; 
+      case "50d":
+        iconName = "hazzy";
+        break; 
+      case "50n":
+        iconName = "hazzy";
+        break; 
+      default:
+        iconName = "cloud";
+    }
+ 
+    return iconName;
+  };
+
   useEffect(() => {
     const getWeather = async () => {
       try {
@@ -46,7 +111,7 @@ const Weather = () => {
         setCurrentWeather({
           temp,
           feels_like,
-          description, 
+          description,
           icon,
         });
       } catch (e) {
@@ -71,8 +136,8 @@ const Weather = () => {
 
   return (
     <div className="flex gap-6 my-4 items-center">
-      <div className="flex items-center gap-2">
-        <img src={`${openWeatherImg}/${currentWeather.icon}.png`} alt="" />
+      <div className="flex items-center gap-4">
+        <img src={`img/icons/${getWeatherIcon(currentWeather.icon)}.svg`} alt="" className="w-8" />
         <div className="text-4xl text-white font-bold flex">
           <span>{`${Number(currentWeather.temp).toFixed(0)}`}</span>
           <span className="text-2xl">{unit.temperature}</span>
