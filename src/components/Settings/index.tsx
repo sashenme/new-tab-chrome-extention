ab71e3d618d8f2d0ba1e1498799767258aeb89f9
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-09 21:14:17 +0100

    style: make setting button

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import CogIcon from '../../assets/icons/CogIcon'
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div>
+      <button className="px-4 py-2 text-lg bg-slate-800/70 text-white rounded-full flex gap-1 backdrop-blur-sm mt-10 hover:bg-slate-900">
+        <CogIcon/>
+        <span className='pr-1'>Settings</span>
+      </button>
+    </div>
   )
 }
 
