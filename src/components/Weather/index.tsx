812565369e99cc70b77004f080b36240fa8aa77c
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-11 00:19:16 +0100

    refactor: add types to the Weather/index.tsx

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { openWeatherApi } from "../../utils/apis";
 // import { unit } from "../../utils/constants";
-import {getWeatherIcon} from "../../utils/functions";
+import { getWeatherIcon } from "../../utils/functions";
 
 interface Main {
   feels_like: number;
@@ -15,28 +15,47 @@ interface WeatherResponse {
   weather: any;
 }
 
-const unit = { name: "metric", temperature: "°C", label:"Celsius" };
+interface currentWeather {
+  temp: number;
+  feels_like: number;
+  description: string;
+  icon: string;
+  main: string;
+}
 
-const Weather = () => {
-  const [error, setError] = useState(false);
-  const [currentWeather, setCurrentWeather] = useState({
-    temp: 1,
-    feels_like: 1,
+interface Location{
+  lat: number;
+  long: number;
+}
+
+const unit = { name: "metric", temperature: "°C", label: "Celsius" };
+
+const initialData = {
+  currentWeather: {
+    temp: 0,
+    feels_like: 0, 
     description: "snowing",
     icon: "03d",
-  });
+    main: "",
+  },
+};
 
-  const [location, setLocation] = useState({ lat: 0, long: 0 });
-  let [url, setUrl] = useState(
-    `${openWeatherApi}&lat=${location.lat}&lon=${location.long}&units=${unit.name}`
-  );
+const Weather = () => {
+  const [currentWeather, setCurrentWeather] = useState<currentWeather>(initialData.currentWeather);
+  const [location, setLocation] = useState<Location>({ lat: 0, long: 0 });
+  const initialUrl = `${openWeatherApi}&lat=${location.lat}&lon=${location.long}&units=${unit.name}`;
+  let [url, setUrl] = useState(initialUrl);
 
   async function http<T>(request: RequestInfo): Promise<T> {
     const response = await fetch(request);
     const body = await response.json();
     return body;
   }
- 
+
+  const roundUpTemp = (temp: number) => {
+    const rounded = Number(temp).toFixed(0);
+    return rounded === "-0" ? 0 : rounded;
+  };
 
   useEffect(() => {
     const getWeather = async () => {
@@ -52,21 +71,16 @@ const Weather = () => {
           feels_like,
           description,
           icon,
+          main,
         });
       } catch (e) {
-        setError(true);
         console.log(e);
       }
     };
     getWeather();
   }, [url, location]);
 
-  const roundUpTemp = temp => {
-   const rounded =  Number(currentWeather.temp).toFixed(0)
-   return rounded === '-0' ? 0 : rounded;
-  }
-
-  useEffect(() => {
+   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
       setLocation({
         lat: position.coords.latitude,
@@ -75,13 +89,17 @@ const Weather = () => {
       setUrl(
         `${openWeatherApi}&lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=${unit.name}`
       );
-    }); 
+    });
   }, []);
 
   return (
     <div className="flex gap-6 my-4 items-center">
       <div className="flex items-center gap-4">
-        <img src={`img/icons/${getWeatherIcon(currentWeather.icon)}.svg`} alt="" className="w-8" />
+        <img
+          src={`img/icons/${getWeatherIcon(currentWeather.icon)}.svg`}
+          alt=""
+          className="w-8"
+        />
         <div className="text-4xl text-white font-bold flex">
           <span>{`${roundUpTemp(currentWeather.temp)}`}</span>
           <span className="text-2xl">{unit.temperature}</span>
