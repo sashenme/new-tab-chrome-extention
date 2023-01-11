278159e2aa0287070d5d775f415325621a80ff25
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-12 00:15:59 +0100

    feat: make add button

--- src/components/Settings/Sections/Timezones.tsx
+++ src/components/Settings/Sections/Timezones.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import PlusIcon from "../../../assets/icons/PlusIcon";
 import Section from "../Section";
 import Timezone from "./Timezones/Timezone";
 
@@ -8,6 +9,7 @@ const Timezones = ({ onToggle, isActive }) => {
       <div className="grid gap-2 mt-4">
         <Timezone name="Asia/Colombo" city="Colombo, Sri Lanka"/>
         <Timezone name="Australia/Melbourne" city="Melbourne, Australia"/>
+        <button className="bg-black hover:bg-gray-900 rounded-full px-4 py-2 flex items-center gap-1 w-full max-w-[8rem] mx-auto mt-4"><PlusIcon/><span>Add timezone</span></button>
       </div>
     </Section>
   );
