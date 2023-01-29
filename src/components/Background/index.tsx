4762ffed9c995614f5031326e397895ae10e73a7
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-29 01:37:26 +0100

    fix: change unsplash url to picsum photos

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Background = () => {
   return (
-    <div className="min-w-screen min-h-screen fixed inset-0 -z-20" style={{background: 'url(https://unsplash.it/1920/1080)'}}>
+    <div className="min-w-screen min-h-screen fixed inset-0 -z-20" style={{background: 'url(https://picsum.photos/1920/1080)'}}>
       <div className="min-w-screen min-h-screen absolute inset-0 bg-slate-900/40"></div>
     </div>
   );
