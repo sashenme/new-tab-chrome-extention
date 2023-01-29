acf80c6f3dc957174cb347b4c273232648a54b7c
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-29 02:48:55 +0100

    style: fix background size

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const Background = () => {
   return (
-    <div className="min-w-screen min-h-screen fixed inset-0 -z-20" style={{background: 'url(https://picsum.photos/1920/1080)'}}>
+    <div
+      className="min-w-screen min-h-screen fixed inset-0 -z-20 bg-cover bg-center bg-slate-900"
+      style={{ backgroundImage: "url(https://picsum.photos/1920/1080)" }}
+    >
       <div className="min-w-screen min-h-screen absolute inset-0 bg-slate-900/40"></div>
     </div>
   );
