4da285c7d0ee800439a8ebc007cfb5ec0e88b8fe
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-29 03:15:26 +0100

    feat: add tailwind peer plugin and style radio buttons

--- src/components/Settings/Elements/Radio.tsx
+++ src/components/Settings/Elements/Radio.tsx
@@ -8,11 +8,30 @@ interface RadioProps {
   onChange: (e: any) => void;
 }
 
-const Radio: React.FC<RadioProps> = ({ name, label, value,checked, onChange }) => {
+const Radio: React.FC<RadioProps> = ({
+  name,
+  label,
+  value,
+  checked,
+  onChange,
+}) => {
   return (
-    <div className="flex gap-2 items-center">
-      <input type="radio" name={name} value={value} id={value} onChange={onChange} checked={checked}/>
-      <label htmlFor={`${value}`} className="text-base">{label}</label>
+    <div className="flex gap-2 items-center mt-1">
+      <div className="relative">
+        <input
+          type="radio"
+          name={name}
+          value={value}
+          id={value}
+          onChange={onChange}
+          checked={checked}
+          className="absolute opacity-0 peer cursor-pointer"
+        />
+        <div className="cursor-pointer w-4 h-4 rounded-full border grid place-content-center border-white peer-checked:border-cyan-500 peer-checked:after:bg-cyan-500 after:block after:h-2 after:w-2 after:rounded-full"></div>
+      </div>
+      <label htmlFor={`${value}`} className="text-base  cursor-pointer">
+        {label}
+      </label>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
@@ -12,5 +13,17 @@ module.exports = {
       ],
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(({ addVariant, e }) => {
+        addVariant('label-checked', ({ modifySelectors, separator }) => {
+            modifySelectors(
+                ({ className }) => {
+                    const eClassName = e(`label-checked${separator}${className}`); // escape class
+                    const yourSelector = 'input[type="radio"]'; // your input selector. Could be any
+                    return `${yourSelector}:checked ~ .${eClassName}`; // ~ - CSS selector for siblings
+                }
+            )
+        })
+    }),
+],
 }
