7054a96961608d09eb843ff85f792093b9bdc686
Sashen Pasindu <sashenpasindu@gmail.com>
2023-01-29 02:44:20 +0100

    style: set setting button to absolute position

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -6,7 +6,7 @@ const Settings = () => {
   const [isPanelOpen, setIsPanelOpen]  = useState<boolean>(false);
   return (
     <div>
-      <button onClick={()=>setIsPanelOpen(true)} className="px-4 py-2 text-base bg-slate-800/70 text-white rounded-full flex items-center gap-1 backdrop-blur-sm mt-10 hover:bg-slate-900 transition-all ease-in duration-75">
+      <button onClick={()=>setIsPanelOpen(true)} className="absolute bottom-6 px-4 py-2 text-base bg-slate-800/70 text-white rounded-full flex items-center gap-1 backdrop-blur-sm mt-10 hover:bg-slate-900 transition-all ease-in duration-75">
         <CogIcon/>
         <span className='pr-1'>Settings</span>
       </button>
